bad4bcce842161830c8a738fbd03e6fcd2106a22
Leigh E Carroll <Leighcarroll@users.noreply.github.com>
2026-03-02 15:56:32 +0100

    Fix base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
 export default defineConfig({
-  base: "/Portfolio-Legacy/",
   plugins: [react()],
+  base: "/Portfolio-Legacy/",
 })
